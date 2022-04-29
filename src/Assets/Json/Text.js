c7f79e9ff11cca250b2d82d8e44fff858f820d5e
sammkinng <lekhrajsaini700@gmail.com>
2022-04-29 15:44:18 +0530

    cleansing

--- src/Assets/Json/Text.js
+++ src/Assets/Json/Text.js
@@ -1,10 +1,10 @@
-import HKintro from '../../Assets/Sound/high_knees/HIGH KNEES_Intro.mp3'
+// import HKintro from '../../Assets/Sound/high_knees/HIGH KNEES_Intro.mp3'
 import HKSCM from '../../Assets/Sound/high_knees/high-knees-start_countdown_merged.mp3'
 
-import JJIntro from '../../Assets/Sound/jumping_jacks/Jumping jacks_intro+start.mp3'
+// import JJIntro from '../../Assets/Sound/jumping_jacks/Jumping jacks_intro+start.mp3'
 import JJIcnt from '../../Assets/Sound/jumping_jacks/Jumping jacks_intro+start+countdown.mp3'
 
-import SIntro from '../../Assets/Sound/squats/squat_tab.mp3'
+// import SIntro from '../../Assets/Sound/squats/squat_tab.mp3'
 import SStart from '../../Assets/Sound/squats/SQUAT_start.mp3'
 
 const data = [
@@ -16,7 +16,7 @@ const data = [
       'Face the camera',
       'Start high knees'
     ],
-    audio: new Audio(HKintro),
+    // audio: new Audio(HKintro),
     aud2: new Audio(HKSCM),
     aud2len: 19600,
     reps: 20
@@ -29,7 +29,7 @@ const data = [
       'Face the camera',
       'Start jumping jacks'
     ],
-    audio: new Audio(JJIntro),
+    // audio: new Audio(JJIntro),
     aud2: new Audio(JJIcnt),
     aud2len: 14000,
     reps: 25
@@ -42,7 +42,7 @@ const data = [
       ' Turn to your left or right, so your profile is visible to the camera',
       'Start squatting',
     ],
-    audio: new Audio(SIntro),
+    // audio: new Audio(SIntro),
     aud2: new Audio(SStart),
     aud2len: 54000,
     reps: 20
